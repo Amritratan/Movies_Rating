7d0ccf1ac2fa63b528d0d7837ae0301f704153c0
SnakeGame <amritratan123@gmail.com>
2023-06-06 18:31:10 +0530

    trying

--- src/main/java/com/Movie_Rating/MovieController.java
+++ src/main/java/com/Movie_Rating/MovieController.java
@@ -1,8 +1,11 @@
 package com.Movie_Rating;
 
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class MovieController {
 }
